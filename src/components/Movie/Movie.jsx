912a937658b9c064e50cbd1ee55a58dddae020e0
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-12-09 06:04:43 +0200

    Handle Routing

---
+++ src/components/Movie/Movie.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Movie extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                Movie
+            </div>
+         );
+    }
+}
+ 
+export default Movie;
--- src/components/elements/Header/Header.jsx
+++ src/components/elements/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import logo from '../../../Assets/Images/reactMovie3_logo.png'
 import './Header.css';
 
@@ -6,7 +7,13 @@ const Header = () => {
     return ( 
         <div className="rmdb-header">
             <div className="rmdb-header-content">
-                <img className="rmdb-logo" src={logo} alt="rmdb"/><span className="header-title-R">R</span><span className="header-title">Movies</span>
+                <Link to='/'>
+                    <div className="rmdb-header-brand">
+                        <img className="rmdb-logo" src={logo} alt="rmdb"/><span className="header-title-R">R</span><span className="header-title">Movies</span>
+                    </div>
+                </Link>
+                
+                
             </div>
         </div>
      );
