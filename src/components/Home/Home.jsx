021f2859c3562537879eeae4dbc765b1e4c68ef0
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-12-09 02:38:16 +0200

    Connecting to api and adding fetchItem method

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {API_KEY,API_TOKEN,API_URL,IMAGE_BASE_URL,POSTER_SIZE,BACKDROP_SIZE} from '../../config';
 import HeroImage from '../elements/HeroImage/HeroImage';
 import SearchBar from '../elements/SearchBar/SearchBar';
 import FourColGrid from '../elements/FourColGrid/ForColGrid';
@@ -8,7 +9,33 @@ import Spinner from '../elements/Spinner/Spinner';
 import './Home.css';
 
 class Home extends Component {
-    state = {  }
+    state = {  
+        movies:[],
+        heroImage: null,
+        loading:false,
+        currentPage:0,
+        totalPages:0,
+        searchItem:''
+    }
+    componentDidMount(){
+        this.setState({loading:true});
+        const endpoint =`${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        this.fetchItems(endpoint);
+    }
+
+    fetchItems = (endpoint) => {
+        fetch(endpoint)
+        .then(result => result.json())
+        .then(result => {
+            const movies =[...this.state.movies, ...result.results];
+            const heroImage = this.state.heroImage || result.results[0];
+            const loading = false;
+            const currentPage = result.page;
+            const totalPages = result.total_pages;
+            this.setState({movies,heroImage,loading,currentPage,totalPages});
+
+        })
+    }
     render() { 
         return ( 
             <div className="rmdb-home">
--- src/config.js
+++ src/config.js
@@ -2,7 +2,9 @@
 // To se the latest configuration fetch it from https://api.themoviedb.org/3/configuration?api_key=019e8f375549e0bbd4a4191862ebc88f
 
 const API_URL = 'https://api.themoviedb.org/3/';
-const API_KEY = '844dba0bfd8f3a4f3799f6130ef9e335';
+const API_KEY = '14625749ffebc61dc14ec79c80f39fd4';
+const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNDYyNTc0OWZmZWJjNjFkYzE0ZWM3OWM4MGYzOWZkNCIsInN1YiI6IjVkZWQ4ZWU5MzI0ODliMDAxMmMyNTg3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OjLmqw25pc_wmvq2Mud1dR0vXHY1JoSBMAO2Z7AC6j8';
+
 
 // Images
 // An image URL looks like this example:
@@ -19,6 +21,7 @@ const POSTER_SIZE = 'w500';
 export {
   API_URL,
   API_KEY,
+  API_TOKEN,
   IMAGE_BASE_URL,
   BACKDROP_SIZE,
   POSTER_SIZE
