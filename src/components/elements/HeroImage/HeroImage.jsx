98d3dd0645de6b2f8c700df25c2472ff456fe7d0
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-12-09 03:03:37 +0200

    enhance heroimage component

--- src/components/elements/HeroImage/HeroImage.jsx
+++ src/components/elements/HeroImage/HeroImage.jsx
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import './HeroImage.css';
 
 const HeroImage = (props) => {
+
+    const styles_background = `linear-gradient(to bottom, rgba(0,0,0,0)39%, rgba(0,0,0,0)41%, rgba(0,0,0,0.65)100%), url('${props.image}'), #1c1c1c`;
     return ( 
-        <div>
-            Hero Image
+        <div className="rmdb-heroimage" style={{background:styles_background}}>
+            <div className="rmdb-heroimage-content">
+                <div className="rmdb-heroimage-text">
+                    <h>{props.title}</h>
+                    <h>{props.text}</h>
+
+                </div>
+            </div>
         </div>
      );
 }
